8d0613da06c24f3448275bb6b0886baa77c5289c
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-12-09 13:53:15 +0600

    Add Report Module components and related assets

--- src/app/report-module.tsx/page.tsx
+++ src/app/report-module.tsx/page.tsx
@@ -1,9 +1,16 @@
 /** @format */
 
+import ReportModule from "@/components/ReportModuleComponents/ReportModule";
 import React from "react";
 
 const ReportModulePage = () => {
-  return <div>ReportModulePage</div>;
+  return (
+    <div className="w-full p-4">
+      <div className="max-w-[2500px] rounded-2xl mx-auto ">
+        <ReportModule />
+      </div>
+    </div>
+  );
 };
 
 export default ReportModulePage;
---
+++ src/components/ReportModuleComponents/ReportModule.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+import StatsSection from "../OverviewComponents/StatsSection";
+
+const ReportModule = () => {
+  return (
+    <div className="w-full bg-transparent space-y-4">
+      <div>
+        <StatsSection />
+      </div>
+    </div>
+  );
+};
+
+export default ReportModule;
