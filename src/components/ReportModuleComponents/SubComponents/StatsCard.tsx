fbc818c05e1ac166a902b5376b09de374370fab1
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-12-09 14:51:01 +0600

    Refactor StatsCard component for improved layout and styling

--- src/components/ReportModuleComponents/SubComponents/StatsCard.tsx
+++ src/components/ReportModuleComponents/SubComponents/StatsCard.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import Image from "next/image";
 import { StatsCardData } from "@/types/ReportModuleTypes";
 
+import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
+
 interface StatsCardProps {
   data: StatsCardData;
 }
@@ -20,8 +22,10 @@ const StatsCard: React.FC<StatsCardProps> = ({ data }) => {
   } = data;
 
   return (
-    <div className={`bg-white rounded-2xl p-6  transition-all hover:shadow-md`}>
-      <div className="flex items-center gap-4">
+    <div
+      className={`bg-white rounded-2xl p-4 space-y-4 transition-all hover:shadow-md`}
+    >
+      <div className="flex items-center gap-4 ">
         <div
           className={` ${bgColor}  w-9 h-9 rounded-xl flex items-center justify-center text-2xl`}
         >
@@ -34,23 +38,27 @@ const StatsCard: React.FC<StatsCardProps> = ({ data }) => {
       <div className="flex justify-between w-full">
         <div className="flex items-center gap-2">
           <span
-            className={`text-sm font-semibold ${
+            className={`text-sm flex items-center font-semibold ${
               trend === "up" ? "text-green-600" : "text-red-600"
             }`}
           >
-            {trend === "up" ? "+" : "-"}
+            {trend === "up" ? (
+              <GoTriangleUp size={20} />
+            ) : (
+              <GoTriangleDown size={20} />
+            )}
             {percentage}%
           </span>
           <Image
             src={trend === "up" ? "/statsUp.png" : "/statsDown.png"}
             alt={trend === "up" ? "Trending up" : "Trending down"}
-            width={20}
-            height={20}
+            width={60}
+            height={40}
             className="object-contain"
           />
         </div>
         <div>
-          <p className="text-3xl font-bold text-gray-900 mt-1">{value}</p>
+          <p className="text-2xl font-bold text-gray-600 mt-1">{value}</p>
         </div>
       </div>
     </div>
