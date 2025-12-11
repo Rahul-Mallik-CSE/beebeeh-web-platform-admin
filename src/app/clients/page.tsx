95621a188d030d29c2fb2ab3e09f6d0bd531a2f8
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-12-11 11:14:04 +0600

    Refactor ClientsPage layout; add border and padding to table section

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -6,8 +6,11 @@ import React from "react";
 const ClientsPage = () => {
   return (
     <div className="w-full p-4">
-      <div className="max-w-[2500px] rounded-2xl mx-auto bg-white">
-        <ClientsTableSections />
+      <div className="max-w-[2500px] rounded-2xl mx-auto space-y-4">
+        {/* table section */}
+        <div className="bg-white border border-gray-200 rounded-2xl p-6">
+          <ClientsTableSections />
+        </div>
       </div>
     </div>
   );
